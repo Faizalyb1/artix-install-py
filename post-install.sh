5e50c003eaf218aef6490fa46b2bb797c15d3ad3
yb1 <112716151+Faizalyb1@users.noreply.github.com>
2022-09-26 22:38:52 +0000

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,1 +1,13 @@
-sudo pacman -S xorg-server xorg-xinit lxappearance firefox xfce4-terminal xterm xorg-xrandr xorg-xbacklight xdg-utils xdg-user-dirs-gtk xorg-xdpyinfo pcmanfm gtk-engine-murrine libgda amd-ucode xf86-video-amdgpu xf86-video-ati ntp-dinit gparted mpv picom qt5ct
+#!/bin/bash
+
+printf '\033c'
+pacman -Sy xorg-server xorg-xinit lxappearance firefox xfce4-terminal xterm xorg-xrandr xorg-xbacklight xdg-utils xdg-user-dirs-gtk xorg-xdpyinfo pcmanfm gtk-engine-murrine libgda amd-ucode xf86-video-amdgpu xf86-video-ati gparted mpv picom qt5ct
+clear
+exit
+sleep 6s
+
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin/
+makepkg -si
+echo "Done"
+sleep 6s
