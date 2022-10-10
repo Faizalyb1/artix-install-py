3e83e7451ff4c24a2175394f8594afa7b5ed338c
yb1 <112716151+Faizalyb1@users.noreply.github.com>
2022-10-10 15:05:38 +0530

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf '\033c'
-sudo pacman -Sy xorg-server xorg-xinit lxappearance firefox xterm mtools dosfstools xorg-xrandr xorg-xbacklight xdg-utils xdg-user-dirs-gtk xorg-xdpyinfo pcmanfm gtk-engine-murrine amd-ucode xf86-video-amdgpu xf86-video-ati gparted mpv picom qt5ct ntp-dinit evince file-roller ttf-hack ttf-joypixels gtk2 gtk3 ttf-nerd-fonts-symbols-2048-em ttf-nerd-fonts-symbols-common awesome-terminal-fonts ttf-jetbrains-mono
+sudo pacman -Sy xorg-server xorg-xinit lxappearance firefox xterm mtools dosfstools xorg-xrandr xorg-xbacklight xdg-utils xdg-user-dirs-gtk xorg-xdpyinfo pcmanfm gtk-engine-murrine amd-ucode xf86-video-amdgpu xf86-video-ati gparted mpv picom qt5ct ntp-dinit evince file-roller 
 clear
 exit
 sleep 6s
