5ef5e322188482ec5aeb5d1770a9c2ebbac99460
yb1 <112716151+Faizalyb1@users.noreply.github.com>
2022-10-10 15:09:46 +0530

    Create fonts.sh

---
+++ fonts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+printf '\033c'
+sudo pacman -S ttf-hack ttf-joypixels gtk2 gtk3 ttf-nerd-fonts-symbols-2048-em ttf-nerd-fonts-symbols-common awesome-terminal-fonts ttf-jetbrains-mono ttf-nerd-fonts-symbols-2048-em-mono
