6c1c1ed45c0ddf33e5fecfe022696ef161a1edad
yb1 <112716151+Faizalyb1@users.noreply.github.com>
2022-10-11 02:10:25 +0530

    Added xdg-user-dirs-update

--- post-install.sh
+++ post-install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 printf '\033c'
-sudo pacman -Sy xorg-server xorg-xinit aria2 lxappearance firefox xterm mtools dosfstools xorg-xrandr xorg-xbacklight xdg-utils xdg-user-dirs-gtk xorg-xdpyinfo pcmanfm gtk-engine-murrine amd-ucode xf86-video-amdgpu xf86-video-ati gparted mpv picom qt5ct ntp-dinit evince file-roller 
+sudo pacman -Sy xorg-server xorg-xinit aria2 lxappearance firefox xterm mtools dosfstools xorg-xrandr xorg-xbacklight xdg-utils xdg-user-dirs xorg-xdpyinfo pcmanfm gtk-engine-murrine amd-ucode xf86-video-amdgpu xf86-video-ati gparted mpv picom qt5ct ntp-dinit evince file-roller 
 clear
 exit
 sleep 6s
+xdg-user-dirs-update
 
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin/
