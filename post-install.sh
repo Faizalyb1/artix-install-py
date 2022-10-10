774ddd8e195fe9aa7cc9ffaa7c2fd48bbb9b495a
yb1 <112716151+Faizalyb1@users.noreply.github.com>
2022-10-10 15:21:39 +0530

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf '\033c'
-sudo pacman -Sy xorg-server xorg-xinit lxappearance firefox xterm mtools dosfstools xorg-xrandr xorg-xbacklight xdg-utils xdg-user-dirs-gtk xorg-xdpyinfo pcmanfm gtk-engine-murrine amd-ucode xf86-video-amdgpu xf86-video-ati gparted mpv picom qt5ct ntp-dinit evince file-roller 
+sudo pacman -Sy xorg-server xorg-xinit aria2 lxappearance firefox xterm mtools dosfstools xorg-xrandr xorg-xbacklight xdg-utils xdg-user-dirs-gtk xorg-xdpyinfo pcmanfm gtk-engine-murrine amd-ucode xf86-video-amdgpu xf86-video-ati gparted mpv picom qt5ct ntp-dinit evince file-roller 
 clear
 exit
 sleep 6s
