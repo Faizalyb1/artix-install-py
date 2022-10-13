431ec1666222e2d0862f38ae51651ab7d79aba6c
yb1 <112716151+Faizalyb1@users.noreply.github.com>
2022-10-14 03:49:17 +0530

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 printf '\033c'
 sudo pacman -Sy xorg-server xorg-xinit aria2 lxappearance firefox xterm mtools dosfstools xorg-xrandr xorg-xbacklight xdg-utils xdg-user-dirs xorg-xdpyinfo pcmanfm gtk-engine-murrine amd-ucode xf86-video-amdgpu gtk2 gtk3 xf86-video-ati gparted mpv qt5ct ntp-dinit mupdf file-roller 
