dcaf34757f55d092c1546b2e337a18c1af23f64d
yb1 <112716151+Faizalyb1@users.noreply.github.com>
2022-10-14 04:11:56 +0530

    Delete fonts.sh

--- fonts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-printf '\033c'
-sudo pacman -S ttf-hack ttf-joypixels ttf-nerd-fonts-symbols-2048-em ttf-nerd-fonts-symbols-common awesome-terminal-fonts ttf-jetbrains-mono ttf-nerd-fonts-symbols-2048-em-mono
-echo "fonts done"
