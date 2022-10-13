df9e0cd59e1a39160949eb8e922702fc792ae23c
yb1 <112716151+Faizalyb1@users.noreply.github.com>
2022-10-14 03:46:08 +0530

    Update fonts.sh

--- fonts.sh
+++ fonts.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 printf '\033c'
-sudo pacman -S ttf-hack ttf-joypixels gtk2 gtk3 ttf-nerd-fonts-symbols-2048-em ttf-nerd-fonts-symbols-common awesome-terminal-fonts ttf-jetbrains-mono ttf-nerd-fonts-symbols-2048-em-mono
+sudo pacman -S ttf-hack ttf-joypixels ttf-nerd-fonts-symbols-2048-em ttf-nerd-fonts-symbols-common awesome-terminal-fonts ttf-jetbrains-mono ttf-nerd-fonts-symbols-2048-em-mono
+echo "fonts done"
