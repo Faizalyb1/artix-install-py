9da85fdd786e85c75501afc8ab705d2dab985c41
yb1 <112716151+Faizalyb1@users.noreply.github.com>
2022-10-14 03:46:24 +0530

    Update fonts.sh

--- fonts.sh
+++ fonts.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 printf '\033c'
 sudo pacman -S ttf-hack ttf-joypixels ttf-nerd-fonts-symbols-2048-em ttf-nerd-fonts-symbols-common awesome-terminal-fonts ttf-jetbrains-mono ttf-nerd-fonts-symbols-2048-em-mono
